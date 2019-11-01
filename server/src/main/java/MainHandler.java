9c5b23174d22d0200ade2d317d12a56300d43676
asbyvshev <barseto4ka@live.ru>
2019-11-01 16:55:58 +0300

    На Сервере: Добавил в классе MainHandler методе channelRead условие получения файла.

--- server/src/main/java/MainHandler.java
+++ server/src/main/java/MainHandler.java
@@ -6,17 +6,24 @@ import io.netty.util.ReferenceCountUtil;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 
 public class MainHandler extends ChannelInboundHandlerAdapter {
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         try {
             if (msg instanceof FileRequest) {
+                System.out.println("запрос на скачивание");
                 FileRequest fr = (FileRequest) msg;
                 if (Files.exists(Paths.get("server/server_storage/" + fr.getFilename()))) {
                     FileMessage fm = new FileMessage(Paths.get("server/server_storage/" + fr.getFilename()));
                     ctx.writeAndFlush(fm);
                 }
+            }
+            if (msg instanceof FileMessage) {
+                System.out.println("прислан файл на запись");
+                FileMessage fm = (FileMessage) msg;
+                Files.write(Paths.get("server/server_storage/" + fm.getFilename()), fm.getData(), StandardOpenOption.CREATE);
             }
         } finally {
             ReferenceCountUtil.release(msg);
