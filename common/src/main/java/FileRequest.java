313fd7ea994105c1bbb624e83a195c11c77f1731
asbyvshev <barseto4ka@live.ru>
2019-11-06 14:33:28 +0300

    1.Добавлен енум с комадами,которые необходимо выолнить  на сервере над файлом. 2.Добавлено поле fileRename - передает новое имя файла. 3.Добавлены геттеры для 1 и 2 пункта. 4.Добавлены новые конструкторы, старый убран.

--- common/src/main/java/FileRequest.java
+++ common/src/main/java/FileRequest.java
@@ -1,11 +1,32 @@
 public class FileRequest extends AbstractMessage {
+
+    public enum Command {
+        DOWNLOAD,DELETE,RENAME,EMPTY
+    }
+    private Command command = Command.EMPTY;
     private String filename;
+    private String fileRename;
 
     public String getFilename() {
         return filename;
     }
 
-    public FileRequest(String filename) {
+    public String getFileRename() {
+        return fileRename;
+    }
+
+    public Command getCommand() {
+        return command;
+    }
+
+    public FileRequest(Command command, String filename) {
+        this.command = command;
         this.filename = filename;
+    }
+
+    public FileRequest(Command command, String filename, String fileRename) {
+        this.command = command;
+        this.filename = filename;
+        this.fileRename = fileRename;
     }
 }
