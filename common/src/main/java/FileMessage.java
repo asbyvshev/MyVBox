4c471d7124795e24752cb435f42daff0d7f6a85f
asbyvshev <barseto4ka@live.ru>
2019-11-08 12:58:46 +0300

    Модифицирован FileMessage для работы с разбитыми на части файлами.

--- common/src/main/java/FileMessage.java
+++ common/src/main/java/FileMessage.java
@@ -4,18 +4,34 @@ import java.nio.file.Path;
 
 public class FileMessage extends AbstractMessage {
     private String filename;
+    private int partNumber;
+    private int partsCount;
     private byte[] data;
 
     public String getFilename() {
         return filename;
     }
 
+    public int getPartNumber() {
+        return partNumber;
+    }
+
+    public int getPartsCount() {
+        return partsCount;
+    }
+
     public byte[] getData() {
         return data;
     }
 
-    public FileMessage(Path path) throws IOException {
+    public void setData(byte[] data) {
+        this.data = data;
+    }
+
+    public FileMessage(Path path, int partNumber, int partsCount) throws IOException {
         filename = path.getFileName().toString();
         data = Files.readAllBytes(path);
+        this.partNumber = partNumber;
+        this.partsCount = partsCount;
     }
 }
