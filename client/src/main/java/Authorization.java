f407dfe84eb6bd109712887bbbfe8752b9b809f1
asbyvshev <barseto4ka@live.ru>
2019-11-21 16:35:27 +0300

    убрал лишний класс

--- client/src/main/java/Authorization.java
+++
@@ -1,8 +0,0 @@
-public class Authorization {
-
-    public static void tryToAuth(String login, String password) {
-        Network.start();
-        AuthRequest authRequest = new AuthRequest(login,password);
-        Network.sendMsg(authRequest);
-    }
-}
